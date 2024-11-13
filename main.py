3ccced3804ea6504bfeadb53b3a8da6d9ae29a2b
premasuthaharan <suthaharanprema@gmail.com>
2024-11-12 16:23:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-79% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for $1.99 $0 for 1 month.
 import flask
 from flask import Flask, render_template, Response
 from picamera.array import PiRGBArray
